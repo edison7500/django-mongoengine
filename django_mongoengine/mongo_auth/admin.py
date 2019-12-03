74aafac6791244e6f397f2066c694a4b8c2c1c08
jiaxin <edison7500@gmail.com>
2019-12-03 18:08:46 +0800

    update mongouser admin

--- django_mongoengine/mongo_auth/admin.py
+++ django_mongoengine/mongo_auth/admin.py
@@ -3,7 +3,8 @@ from .models import MongoUser
 
 
 class UserAdmin(admin.DocumentAdmin):
-    list_display = ["username", "email", ]
+    add_form_template = 'admin/auth/user/add_form.html'
+    list_display = ["username", "email", "date_joined", ]
 
 
 admin.site.register(MongoUser, UserAdmin)
